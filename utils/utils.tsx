773eca921f5f4f4f236fa33bcad9b1c08b92a809
yahia008 <yahyatijjani99@gmail.com>
2024-10-04 10:08:14 -0700

    safe

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,5 +1,5 @@
 import { Connection, clusterApiUrl } from '@solana/web3.js';
-import { Metaplex, walletAdapterIdentity } from '@metaplex-foundation/js';
+import { Metaplex, walletAdapterIdentity, toBigNumber} from '@metaplex-foundation/js';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import BN from 'bn.js'
 
@@ -13,6 +13,7 @@ export const mintNFT = async (wallet: any, uri: string, input:string) => {
     // Initialize Metaplex with the connection and wallet
     const metaplex = Metaplex.make(connection)
         .use(walletAdapterIdentity(wallet))
+        console.log('Connected to Solana network and initialized Metaplex.');
           // Using Bundlr for storage, can be configured to your needs
 
     // Create the NFT
@@ -22,11 +23,12 @@ export const mintNFT = async (wallet: any, uri: string, input:string) => {
             uri: uri, // IPFS URL from NFT.Storage
             name: input,
             sellerFeeBasisPoints: 500, // 5% royalty
-            
+            maxSupply: toBigNumber(1),
              // Number of copies allowed, set to 1 for a unique NFT
         });
 
     console.log('NFT Minted: ', nft);
+    return nft
 };
 
 
